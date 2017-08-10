febadf2d500f5dff8b712bef6625d688a618afb7
Mohammad Hader <haderm94@hotmail.com>
2017-08-10 15:22:18 +0300

    after language

--- app/src/main/java/com/ayesh/muhammad/tasbeh/LanguageActivity.java
+++ app/src/main/java/com/ayesh/muhammad/tasbeh/LanguageActivity.java
@@ -1,70 +1,88 @@
 package com.ayesh.muhammad.tasbeh;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
 
-import com.ayesh.muhammad.tasbeh.services.MyContextWrapper;
+import com.ayesh.muhammad.tasbeh.adapter.CustomAdapter;
+import com.ayesh.muhammad.tasbeh.model.ListItem;
 
-import java.util.Locale;
+import java.util.ArrayList;
 
 
 public class LanguageActivity  extends AppCompatActivity{
+    private ListView listView;
+    private CustomAdapter customAdapter;
+    private ArrayList<ListItem> arrayList;
+    private String[] languageList =new String[] {
+            "Arabic",
+            "English"
+            };
 
+    private int[] flags =new int[] {
+            R.drawable.arabic,
+            R.drawable.english
+           };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+        listView = (ListView) findViewById(R.id.listView);
+        arrayList = new ArrayList<ListItem>();
 
-        final Button arabicBtn = (Button)findViewById(R.id.arabicBtn);
-        arabicBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        for (int i = 0;i <=1;i++){
 
-                String languageToLoad  = "ar"; // your language
-                Locale locale = new Locale(languageToLoad);
-                Locale.setDefault(locale);
-                Configuration config = new Configuration();
-                config.locale = locale;
-                getBaseContext().getResources().updateConfiguration(config,
-                        getBaseContext().getResources().getDisplayMetrics());
+            ListItem listItem = new ListItem();
+            listItem.setLanguageName(languageList[i]);
+            listItem.setLanguageImage(flags[i]);
 
-                SharedPreferences arabicgepref = getSharedPreferences("language",MODE_PRIVATE);
-                SharedPreferences.Editor editor = arabicgepref.edit();
-                editor.putString("languageToLoad",languageToLoad );
-                editor.commit();
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                i.putExtra("Language", "arabic");
-                startActivity(i);
-            }
-        });
-        final Button englishBtn = (Button)findViewById(R.id.englishBtn);
-        englishBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String languageToLoad  = "en"; // your language
-                Locale locale = new Locale(languageToLoad);
-                Locale.setDefault(locale);
-                Configuration config = new Configuration();
-                config.locale = locale;
-                getBaseContext().getResources().updateConfiguration(config,
-                        getBaseContext().getResources().getDisplayMetrics());
-
-                SharedPreferences languagepref = getSharedPreferences("language",MODE_PRIVATE);
-                SharedPreferences.Editor editor = languagepref.edit();
-                editor.putString("languageToLoad",languageToLoad );
-                editor.commit();
-
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                i.putExtra("Language", "english");
-                startActivity(i);
-            }
-        });
+            arrayList.add(listItem);
+        }
+        customAdapter = new CustomAdapter(this,arrayList);
+        listView.setAdapter(customAdapter);
+//
+//        final Button arabicBtn = (Button)findViewById(R.id.arabicBtn);
+//        arabicBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//
+//                String languageToLoad  = "ar"; // your language
+//                Locale locale = new Locale(languageToLoad);
+//                Locale.setDefault(locale);
+//                Configuration config = new Configuration();
+//                config.locale = locale;
+//                getBaseContext().getResources().updateConfiguration(config,
+//                        getBaseContext().getResources().getDisplayMetrics());
+//
+//                SharedPreferences arabicgepref = getSharedPreferences("language",MODE_PRIVATE);
+//                SharedPreferences.Editor editor = arabicgepref.edit();
+//                editor.putString("languageToLoad",languageToLoad );
+//                editor.commit();
+//                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+//                i.putExtra("Language", "arabic");
+//                startActivity(i);
+//            }
+//        });
+//        final Button englishBtn = (Button)findViewById(R.id.englishBtn);
+//        englishBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                String languageToLoad  = "en"; // your language
+//                Locale locale = new Locale(languageToLoad);
+//                Locale.setDefault(locale);
+//                Configuration config = new Configuration();
+//                config.locale = locale;
+//                getBaseContext().getResources().updateConfiguration(config,
+//                        getBaseContext().getResources().getDisplayMetrics());
+//
+//                SharedPreferences languagepref = getSharedPreferences("language",MODE_PRIVATE);
+//                SharedPreferences.Editor editor = languagepref.edit();
+//                editor.putString("languageToLoad",languageToLoad );
+//                editor.commit();
+//
+//                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+//                i.putExtra("Language", "english");
+//                startActivity(i);
+//            }
+//        });
 
     }
 
---
+++ app/src/main/java/com/ayesh/muhammad/tasbeh/model/ListItem.java
@@ -0,0 +1,31 @@
+package com.ayesh.muhammad.tasbeh.model;
+
+/**
+ * Created by mhader on 8/10/17.
+ */
+/**
+ * Created by prats on 5/28/2015.
+ */
+public class ListItem {
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
+
+
+    public int getLanguageImage() {
+        return languageImage;
+    }
+
+    public void setLanguageImage(int countryImage) {
+        this.languageImage = countryImage;
+    }
+
+    private String languageName;
+    private int languageImage;
+}
+
